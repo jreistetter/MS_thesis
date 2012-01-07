ad3278763e7ad252c00cc5b035367ed8b379bb4f
Joe Reistetter <jreistetter@gmail.com>
2012-01-06 18:27:56 -0800

    added more to GEO script

--- exprs/process_expr_data.R
+++ exprs/process_expr_data.R
@@ -8,3 +8,12 @@ setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/data/exprs")
 geos <- c("GSE8839", "GSE16146", "GSE10391", "GSE8786", "GSE9331")
 
 gse8839 <- getGEO(geos[1], destdir=".")
+
+#This particular GSE has two platforms, getGEO returned a list containing both of them.
+#Separate them out to two variables
+
+#GPL2787 
+gse.plat1 <- gse8839[[1]]
+#GPL5714
+gse.plat2 <- gse8839[[2]]
+
