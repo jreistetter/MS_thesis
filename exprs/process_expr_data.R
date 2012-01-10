3293b4cc9bfea9b1dad6ea54ab1b1639c77452a8
Joe Reistetter <jreistetter@gmail.com>
2012-01-09 19:45:11 -0800

    Realized need to download and process raw files for expression, doing that

--- exprs/process_expr_data.R
+++ exprs/process_expr_data.R
@@ -1,21 +1,50 @@
-#Script to download the 5 GEO datasets that will be used to build the PMN.
+#Script to download the supplementary files (raw data) for 5 GEO datasets 
+#that will be used to build the PMN.
+
+#Notes about the datasets:
+#GSE8786 - Raw data good, text format
+#GSE8839 - appears to be corrupted (some columns shifted in raw data)
+#GSE9331 - Raw data good, genepix file format (!) (.gpr)
+          #It's 3 different platforms, and 4921 says channel 1 is cy5.
+          #Need to check this later to see if just entry error in GEO
+          #gpl4291 ch1 = cy5 = red
+          #gpl4293 ch1 = cy3 = green
+          #gpl5774 ch1 = cy3 = green
+#GSE10391 - Raw data good, text format
+#GSE16146 - get softfile via getGEO, raw data not available separately
+
 
 library(GEOquery)
+library(limma)
 
 #At OHSU wd
 setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/data/exprs")
 #My laptop wd
 setwd("~/schoolDB/Dropbox/thesis_work/data/exprs")
 
+##Set the column IDs for reading each dataset into the limma object
+#GSE8786
+#column names: Ch1 Intensity (Mean), Ch1 Background (Median), Ch2 Background (Median), Ch2 Intensity (Mean) -- ch1 = Cy3 = green, ch2 = Cy5 = red
+g.8786.cols <- list(R="Ch2 Intensity (Mean)", G="Ch1 Intensity (Mean)",
+                    Rb="Ch2 Background (Median)", Gb="Ch1 Background (Median)")
 
-#GEO Numbers
-geos <- c("GSE8839", "GSE16146", "GSE10391", "GSE8786", "GSE9331")
 
-gse8786.raw <- getGEO(filename="GSE8786_RAW.tar")
+#GEO IDs for the five datasets
+GEO.ids <- c("GSE8839", "GSE16146", "GSE10391", "GSE8786", "GSE9331")
 
-gse8839 <- getGEO(geos[1], destdir=".")
-gse8839.supp <- getGEOSuppFiles(geos[1])
-gse8839.raw <- getGEO(filename="GSE8839_RAW.tar")
+#Download the supplementary data (raw files) for each dataset
+i=1
+dl.file.info <- list()
+for(i in 1:length(GEO.ids)){
+  dl.file.info[[GEO.ids[i]]] <- getGEOSuppFiles(GEO.ids[i], makeDirectory=F)
+}
+
+#This is returning a workable file, need to use the GEO tutorial to extract it
+g.16146 <- getGEO(GEO.ids[2], destdir="./", GSEMatrix=F)
+Table(GSMList(g.16146)[[1]])[1:5,]
+
+#Probably will loop through each element of GSMList and write out to text file,
+#then read back in (or maybe just make into new Dfs)
 
 #This particular GSE has two platforms, getGEO returned a list containing both of them.
 #Separate them out to two variables
@@ -34,7 +63,7 @@ plat1.sampleIDs <- sampleNames(plat1.pheno) #get the sample names
 plat1.feats <- featureData(gse.plat1) #annotated dframe containing info about probes
 plat1.probeIDs <- featureNames(plat1.feats) #probeIDs
 plat1.pdata <- pData(plat1.feats) #info about probeID, including RV#
-
+plat1.assay <- assayData(gse.plat1)
 
 
 
