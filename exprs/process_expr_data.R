c4b95b5f75fe519b12c039f88d1b46844104cd43
Joe Reistetter <jreistetter@gmail.com>
2012-01-15 12:37:22 -0800

    Finalized the normalization, still need to do DE

--- exprs/process_expr_data.R
+++ exprs/process_expr_data.R
@@ -19,7 +19,7 @@ library(GEOquery)
 library(limma)
 
 #At OHSU wd
-setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/data/exprs")
+#setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/data/exprs")
 #My laptop wd
 setwd("~/schoolDB/Dropbox/thesis_work/data/exprs/GSE8786/")
 
@@ -33,7 +33,6 @@ setwd("~/schoolDB/Dropbox/thesis_work/data/exprs/GSE8786/")
 #First, need to create Targets frame for use in the read.maimages func
 
 #Pull info from the GEO repository so we can get annotations for each array
-dir.create("./GS8786")
 g.8786.geo <- getGEO("GSE8786", destdir="./") #Downloads the soft files
 g.8786.pheno <- phenoData(g.8786.geo[[1]]) #Gets phenotype data
 g.8786.pdata <- pData(g.8786.pheno) #Dataframe of all data associated with arrays
@@ -54,6 +53,7 @@ g.8786.treatment <- as.character(g.8786.pdata$description.1)
 
 #Function to parse out the treatment from the description
 text_to_label <- function(row){
+  #Control logic to separate control from treatments
   if (grepl("Control", row, fixed=T)){
     row.split <- strsplit(row, " ", fixed=T)
     n.days <- row.split[[1]][8]
@@ -70,6 +70,9 @@ text_to_label <- function(row){
 
 g.8786.treatment <- sapply(g.8786.treatment, text_to_label, USE.NAMES=F)
 
+
+#All the data is in the correct format, build it into a dataframe
+#that fits the specs for input into the read.maimages function.
 g.8786.targets <- data.frame(FileName=g.8786.fnames,
                              Cy3 = rep("control", length(g.8786.fnames)),
                              Cy5 = g.8786.treatment)
@@ -88,15 +91,32 @@ g.8786.rg <- read.maimages(g.8786.targets, columns=g.8786.cols,
                            path="./GSE8786_RAW")
 colnames(g.8786.rg$genes) <- c("Block", "Row", "Column", "Spot", "Name")
 
-##Now that the data is read in, do some QA/QC
-dir.create("../QA/prenormMA")
-plotMA3by2(g.8786.rg, path="../QA/prenormMA")
+g.8786.rg$printer <- getLayout(g.8786.rg$genes)
 
-#Normalize the arrays
+##Now that the data is read in, do some QA/QC by looking at MA plots
+dir.create("./QA/prenormMA")
+plotMA3by2(g.8786.rg, path="./QA/prenormMA")
+
+
+
+#The filtered helped remove some of the artifacts, but some odd
+#patterns (diagonal lines fanning out from the lower left quadrant
+#of the plots)
+
+#Normalize the arrays, may have to remove some if the artifacts remain
 g.8786.bc <- backgroundCorrect(g.8786.rg, method="normexp", offset=50)
-g.8786.MA <- normalizeWithinArrays(g.8786.rg, method="loess")
-g.8786.MAq <- normalizeBetweenArrays(g.8786.rg, method="Aquantile")
+g.8786.bc.norm <- normalizeWithinArrays(g.8786.bc)
+g.8786.bc.norm <- normalizeWithinArrays(g.8786.bc, method="loess")
 
+#Need to filter so that only RV genes are present
+g.8786.rv.idx <- grepl(pattern="RV", x=g.8786.rg$genes$Name, fixed=T)
+sum(g.8786.rv.idx) #3924 genes represented
+
+g.8786.bc.norm.rv <- g.8786.bc.norm[g.8786.rv.idx,]
+
+#Redo the MA plots and see if artifacts disappear
+dir.create("./postnormMA_RVfiltered")
+plotMA3by2(g.8786.bc.norm.rv, path="./postnormMA_RVfiltered")
 
 #QA plots to see if normalization worked
 setwd("./QA")
@@ -105,57 +125,7 @@ plotDensities(g.8786.rg)
 dev.off()
 
 png("corrected_densities.png")
-plotDensities(g.8786.MA)
+plotDensities(g.8786.bc.norm.rv)
 dev.off()
 
-plotDensities(g.8786.MAq)
-
-dir.create("./postnormMA")
-plotMA3by2(g.8786.MA, path="./postnormMA")
-
-
-
-#GEO IDs for the five datasets
-GEO.ids <- c("GSE8839", "GSE16146", "GSE10391", "GSE8786", "GSE9331")
-
-#Download the supplementary data (raw files) for each dataset
-i=1
-dl.file.info <- list()
-for(i in 1:length(GEO.ids)){
-  dl.file.info[[GEO.ids[i]]] <- getGEOSuppFiles(GEO.ids[i], makeDirectory=F)
-}
-
-#This is returning a workable file, need to use the GEO tutorial to extract it
-g.16146 <- getGEO(GEO.ids[2], destdir="./", GSEMatrix=F)
-Table(GSMList(g.16146)[[1]])[1:5,]
-
-#Probably will loop through each element of GSMList and write out to text file,
-#then read back in (or maybe just make into new Dfs)
-
-#This particular GSE has two platforms, getGEO returned a list containing both of them.
-#Separate them out to two variables
-
-#GPL2787 
-gse.plat1 <- gse8839[[1]]
-#GPL5714
-gse.plat2 <- gse8839[[2]]
-
-#First platform (GPL2787)
-#See what data is stored in the ExpressionSet
-plat1.pheno <- phenoData(gse.plat1) #contains sampleIDs, info about experiment blank
-plat1.sampleIDs <- sampleNames(plat1.pheno) #get the sample names
-
-#Take a look at the feature data
-plat1.feats <- featureData(gse.plat1) #annotated dframe containing info about probes
-plat1.probeIDs <- featureNames(plat1.feats) #probeIDs
-plat1.pdata <- pData(plat1.feats) #info about probeID, including RV#
-plat1.assay <- assayData(gse.plat1)
-
-
-
-
-
-
-
-
-probesets <- as.character(Table(GSMList(gse.plat1)[[1]])$ID_REF)
+plotDensities(g.8786.MAq.rv)
