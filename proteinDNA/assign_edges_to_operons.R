0ec604b71cc21874c3d1f9f5fd5e1eb41a0bf502
Joe Reistetter <jreistetter@gmail.com>
2012-02-18 15:46:09 -0800

    Data imported, putting into objects for analysis

--- proteinDNA/assign_edges_to_operons.R
+++ proteinDNA/assign_edges_to_operons.R
@@ -12,6 +12,47 @@
 # Written by Joe Reistetter
 
 # -import both data
-# -get H37Rv annotation from biomaRt
+
+#At OHSU Dropbox
+#setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/")
+#My laptop Dropbox
+setwd("~/schoolDB/Dropbox/thesis_work/data/ChIP-Seq/")
+reg_targ <- read.table("peaks_2012-01-06_18-35-16.tsv", sep="\t",
+                        head=T, stringsAsFactors=F, 
+                       colClasses=c("character", "character", "NULL","NULL","NULL",
+                                      "numeric", "numeric", "NULL","NULL","NULL")
+                       )
+head(reg_targ)
+
+operons <- read.table("tbdb_operons.txt", sep=",", head=T, stringsAsFactors=F,
+                      colClasses=c(rep("NULL",3), "numeric", "numeric",
+                                   "character", rep("NULL",5)),
+                      quote=""
+                      )
+
+nrow(operons) == 2525 #Number of operons downloaded from tbdb.org
+
+# -load H37Rv annotation obtained from tbdb.org
+h37rv.annot <- read.table("tbdb_H37rv_annotation.txt", sep="\t", head=T, stringsAsFactors=F,
+                          colClasses=c(rep("NULL",3), "numeric", "numeric", "character",
+                                       rep("NULL",3), "character", rep("NULL",3)),
+                          quote=""
+                          )
+
+nrow(h37rv.annot) == 3999 #3999 is how many genes tbdb.org said were downloaded
+
 # -use gene start/stop and operon start/stop coords to assign genes to operons
+library(IRanges)
+operons.minus <- operons[operons$Strand=="-",]
+genes.minus <- h37rv.annot[h37rv.annot$Strand=="-",]
+
+o.minus.r <- RangedData(ranges=IRanges(start = operons.minus$Start,
+                                    end = operons.minus$Stop),
+                                    space = 1)
+  
+  RangedData(ranges = IRanges(start = start,
+                              + end = end), strand = strand, space = chr)
+
+
+#  making sure that the gene and operon are on the same strand
 # -for each gene in an operon, create an interaction between regulator and gene (protein-DNA edge)
