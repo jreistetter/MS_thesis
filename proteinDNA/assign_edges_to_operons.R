9dc02d7fce4085a2958537f3da38ba9088f9da8c
Joe Reistetter <jreistetter@gmail.com>
2012-02-18 16:01:12 -0800

    Data into structures, genes assigned to operons

--- proteinDNA/assign_edges_to_operons.R
+++ proteinDNA/assign_edges_to_operons.R
@@ -42,17 +42,47 @@ h37rv.annot <- read.table("tbdb_H37rv_annotation.txt", sep="\t", head=T, strings
 nrow(h37rv.annot) == 3999 #3999 is how many genes tbdb.org said were downloaded
 
 # -use gene start/stop and operon start/stop coords to assign genes to operons
+#  do minus and plus strands separate
 library(IRanges)
+
+# Minus strand
 operons.minus <- operons[operons$Strand=="-",]
 genes.minus <- h37rv.annot[h37rv.annot$Strand=="-",]
 
 o.minus.r <- RangedData(ranges=IRanges(start = operons.minus$Start,
                                     end = operons.minus$Stop),
                                     space = 1)
-  
-  RangedData(ranges = IRanges(start = start,
-                              + end = end), strand = strand, space = chr)
+g.minus.r <- RangedData(ranges=IRanges(start = genes.minus$Start,
+                                       end = genes.minus$Stop),
+                        space = 1)
 
+minus.overlaps <- as.data.frame(as.matrix(findOverlaps(g.minus.r, o.minus.r)))
+colnames(minus.overlaps) <- c("gene", "operon")
+minus.overlaps <- minus.overlaps[with(minus.overlaps, order(operon)),]
+
+# Positive strand
+operons.plus <- operons[operons$Strand=="+",]
+genes.plus <- h37rv.annot[h37rv.annot$Strand=="+",]
+
+o.plus.r <- RangedData(ranges=IRanges(start = operons.plus$Start,
+                                      end = operons.plus$Stop),
+                       space = 1)
+g.plus.r <- RangedData(ranges=IRanges(start = genes.plus$Start,
+                                      end = genes.plus$Stop),
+                       space = 1)
+
+plus.overlaps <- as.data.frame(as.matrix(findOverlaps(g.plus.r, o.plus.r)))
+colnames(plus.overlaps) <- c("gene", "operon")
+plus.overlaps <- plus.overlaps[with(plus.overlaps, order(operon)),]
+
+#Make sure that the splits worked
+nrow(h37rv.annot) == (nrow(genes.minus) + nrow(genes.plus))
+nrow(operons) == (nrow(operons.minus) + nrow(operons.plus))
+
+
+#Now need to assign regulators to operons
+
+dd[with(dd, order(-z, b)), ]
 
 #  making sure that the gene and operon are on the same strand
 # -for each gene in an operon, create an interaction between regulator and gene (protein-DNA edge)
