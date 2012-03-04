f1261067b9474c1a6c13fcbb72e152e2a47e998b
Joe Reistetter <jreistetter@gmail.com>
2012-03-03 16:59:34 -0800

    Discretization of arrays finished

--- exprs/exprs_to_PMN.R
+++ exprs/exprs_to_PMN.R
@@ -4,7 +4,8 @@
 # 1 - Load expression data from RData objects
 # 2 - Merge the data frames from each experiment
 # 3 - Exclude arrays identified in QC as erroneous
-# 4 - Discretize data at a given fold change
+# 4 - Discretize data at a fold thresholds {1, 1.5, 2}
+#     -Filter out arrays with less than 10 DE genes
 # 5 - Write the combined dataframe to a PMN file
 
 # 1 - Load expression data from RData objects
@@ -47,8 +48,8 @@ expr <- merge(expr, gpl.8523.rv.M,
 expr <- merge(expr, gpl.8562.rv.M,
               by.x="Row.names", by.y="row.names")
 
-#save for later use
-save(expr, file="expr.RData")
+#Get rid of Row.names column
+expr <- expr[,-1]
 
 #get rid of all objects except expr
 objs <- ls()
@@ -73,8 +74,46 @@ expr <- expr[,-cols.excl]
 #16, all the arrays excluded
 
 
+#save for later use
+save(expr, file="expr.RData")
+
+
 # 4 - Discretize data at a given fold change
+source("../../code/exprs/exprs_funcs.R")
+
+expr.1 <- discretize(expr, 1)
+ncol(expr.1)
+#128 arrays
+
+#Filter out arrays with less than 10 DE genes, 9 arrays will be removed
+lt.10 <- which(unlist(lapply(expr.1, function(x) sum(abs(x), na.rm=t))) < 10)
+expr.1 <- expr.1[,-lt.10]
+128 - ncol(expr.1)
+#9 arrays removed
+
+expr.1.5 <- discretize(expr, 1.5)
+ncol(expr.1.5)
+#128 arrays
+
+#Filter out arrays with less than 10 DE genes, 9 arrays will be removed
+lt.10 <- which(unlist(lapply(expr.1.5, function(x) sum(abs(x), na.rm=t))) < 10)
+expr.1.5 <- expr.1.5[,-lt.10]
+128 - ncol(expr.1.5)
+#23 arrays removed
+
+expr.2 <- discretize(expr, 2)
+ncol(expr.2)
+#128 arrays
+
+#Filter out arrays with less than 10 DE genes, 9 arrays will be removed
+lt.10 <- which(unlist(lapply(expr.2, function(x) sum(abs(x), na.rm=t))) < 10)
+expr.2 <- expr.2[,-lt.10]
+128 - ncol(expr.2)
+#31 arrays removed
+
 # 5 - Write the combined dataframe to a PMN file
+
+
 
 
 
