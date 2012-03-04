626deb6247d15904c15b400156a21d1392b00966
Joe Reistetter <jreistetter@gmail.com>
2012-03-03 16:03:20 -0800

    Data merged

--- exprs/exprs_to_PMN.R
+++ exprs/exprs_to_PMN.R
@@ -22,19 +22,39 @@ load("GSE8786/g.8786.M.rv.RData")
 load("GSE9331/gpl.4291.M.avg.RData")
 load("GSE9331/gpl.4293.M.avg.RData")
 load("GSE16146/gpl.8523.M.RData")
-load("GSE16146/gpl.8561.M.RData")
+#Dont use 8561 until duplication issue is resolved
+#load("GSE16146/gpl.8561.M.RData")
 load("GSE16146/gpl.8562.M.RData")
 
 
 # 2 - Merge the data frames from each experiment
-expr <- merge()
 
+rownames(g.8786.M.rv) <- toupper(rownames(g.8786.M.rv))
+rownames(gpl.4291.M.avg) <- toupper(rownames(gpl.4291.M.avg))
+rownames(gpl.4293.M.avg) <- toupper(rownames(gpl.4293.M.avg))
+rownames(gpl.8523.rv.M) <- toupper(rownames(gpl.8523.rv.M))
+rownames(gpl.8562.rv.M) <- toupper(rownames(gpl.8562.rv.M))
 
+expr <- merge(g.8786.M.rv, gpl.4291.M.avg,
+              by.x="row.names", by.y="row.names")
 
+expr <- merge(expr, gpl.4293.M.avg,
+              by.x="Row.names", by.y="row.names")
 
+expr <- merge(expr, gpl.8523.rv.M,
+              by.x="Row.names", by.y="row.names")
 
+expr <- merge(expr, gpl.8562.rv.M,
+              by.x="Row.names", by.y="row.names")
 
+#save for later use
+save(expr, "expr.RData")
 
+#get rid of all objects except expr
+objs <- ls()
+objs[which(objs == "expr")] <- "objs"
+
+rm(list=objs)
 
 # 3 - Exclude arrays identified in QC as erroneous
 # 4 - Discretize data at a given fold change
@@ -49,5 +69,23 @@ expr <- merge()
 
 
 
+
+
+
+
+expr.objs <- ls()
+
+mmerge <- function(obj.names){
+  command <- sprintf('merge(%s, %s, by.x="row.names", by.y="row.names")', obj.names[1], obj.names[2])
+  df <- eval(parse(text=command))
+  for (i in c(3:length(obj.names))){
+    command <- sprintf('merge(df, %s, by.x="row.names", by.y="row.names")', obj.names[i])
+    df <- eval(parse(text=command))
+  }
+  
+  return(df)
+}
+
+expr <- mmerge(expr.objs)
 
 
