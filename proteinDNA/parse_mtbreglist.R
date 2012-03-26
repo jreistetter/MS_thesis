2dd5857a2084408dce274f98bc0293d84fbc7b14
Joe Reistetter <jreistetter@gmail.com>
2012-03-26 14:51:15 -0700

    File read in and filtered to just regulators and promoters, removed consensus sequences

--- proteinDNA/parse_mtbreglist.R
+++ proteinDNA/parse_mtbreglist.R
@@ -2,3 +2,21 @@
 # Pierre-E. Jacques [jacquespe@gis.a-star.edu.sg] on March 20, 2012 at
 # reistett@ohsu.edu
 
+#OHSU
+
+#Laptop
+setwd('~/schoolDB/Dropbox/thesis_work/')
+
+#Read in flat file
+dat.raw <- read.table('./data/protein-DNA/MtbRegList-litt_R1-1.txt',
+                      head=F, stringsAsFactors=F, quote='', sep='\t')
+
+#Not interested in TSS, just regulators and promoters. Select those.
+dat.regs <- dat.raw[dat.raw[,1]%in%c("Regulateur", "Promoteur"),c(2,3)]
+
+#Don't care about consensus sequence, just want reg-target pairs. So
+#filter out those.
+consensus.idx <- which(toupper(dat.regs[,2])=="CONSENSUS")
+dat.filtered <- dat.regs[-consensus.idx,]
+
+
