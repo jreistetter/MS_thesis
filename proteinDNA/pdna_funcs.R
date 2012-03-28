fcfe7ff33e00d67f9a18271d1ca5402aafee6197
Joe Reistetter <jreistetter@gmail.com>
2012-03-27 19:44:30 -0700

    Added function to assign pDNA edges from reg/targs and operons

--- proteinDNA/pdna_funcs.R
+++ proteinDNA/pdna_funcs.R
@@ -1,6 +1,52 @@
 # Script containing support functions for the parsing, processing, and writing
 # of the pDNA interaction data.
 
+
+assign_pDNA_edge <- function(reg_target, genes, op_list){
+  #Assigns edges between regulator and targets, and members of operons' targets
+  #that are downstream of target
+  #Params:
+#   reg_target - df, 1st col is regulator, 2nd target
+#   genes - df, 1st col is operon ID corresponding to list name, 2nd is gene
+#   op_list - list of vectors with operon members, key is the operon ID from genes
+  
+  pDNA_edges <- as.matrix(data.frame(protein=vector(mode="character"),
+                                     gene=vector(mode="character"),
+                                     stringsAsFactors=F))
+  
+  for (i in 1:nrow(reg_target)){
+    target <- reg_target[i,2]
+    
+    #If target isn't in an operon, just write that pair
+    if(!(target%in%genes[,2])){
+      pDNA_edges <- rbind(pDNA_edges, c(reg_target[i,1], target))
+      next
+    }
+    
+    op.id <- genes[genes[,2]==target,1]
+    op.vec <- op_list[[op.id]]
+    #Get index of target in operon
+    target.idx <- which(toupper(op.vec)==target)
+    #Check if on complement strand
+    complement = grepl("C", target, ignore.case=TRUE)
+    
+    #If on c strand, need to add target and all 
+    #genes to the "left" of the target
+    if (complement){
+      for (j in 1:target.idx){
+        pDNA_edges <- rbind(pDNA_edges, c(reg_target[i,1], op.vec[j]))
+      }
+      next
+    }
+    
+    #If not on c strand, add target and all genes to "right" of target
+    for (k in target.idx:length(op.vec)){
+      pDNA_edges <- rbind(pDNA_edges, c(reg_target[i,1], op.vec[k]))
+    }
+  }
+  return(pDNA_edges)
+}
+
 operons_from_gene_pairs <- function(op.df, gene.cols){
   #Params
   #op.df - data frame with operon gene pairs as two columns
