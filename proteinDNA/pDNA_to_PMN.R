991c47d3be25c6e5bd9baa0a29c4f2195fc001af
Joe Reistetter <jreistetter@gmail.com>
2012-03-27 17:57:48 -0700

    Imported regulator/targets, combined into one df

--- proteinDNA/pDNA_to_PMN.R
+++ proteinDNA/pDNA_to_PMN.R
@@ -22,3 +22,24 @@
 #   At this time, too worried about non-independence of operon predictions to do
 #   a confidence score based on the number of DBs edge is in.
 
+#At OHSU Dropbox
+#root_path <-"/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work"
+#My laptop Dropbox
+root_path <- "~/schoolDB/Dropbox/thesis_work"
+
+setwd(paste(root_path, "/data/protein-DNA/RData", sep=""))
+source(paste(root_path, "/code/proteinDNA/pdna_funcs.R", sep=""))
+
+#Import regulator/target pairs
+load("mtbreglist.pairs.RData")
+load("myco.pairs.TFBS.RData")
+load("myco.pairs.ortho.RData")
+
+reg_target <- rbind(mtbreglist.pairs, myco.pairs.TFBS)
+reg_target <- rbind(reg_target, myco.pairs.ortho)
+
+nrow(reg_target) #584 pairs
+
+
+
+
