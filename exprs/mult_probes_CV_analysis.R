73ae77cd73be77b12507567ae8313cdc5b51654f
Joe Reistetter <jreistetter@gmail.com>
2012-04-06 11:30:45 -0700

    Added plots showing CV vs M and A for GPL4291

--- exprs/mult_probes_CV_analysis.R
+++ exprs/mult_probes_CV_analysis.R
@@ -3,7 +3,12 @@
 # 
 # Written by Joe Reistetter
 
+#ohsu
+db <- "~/Dropbox/thesis_work/"
+#laptop
+#db <- "~/schoolDB/Dropbox/thesis_work/"
 
+source(paste(db, "code/exprs/exprs_funcs.R", sep=""))
 
 ######################################################
 #
@@ -12,6 +17,13 @@
 #
 ######################################################
 
+path.4291 <- paste(db, "data/exprs/GSE9331/gpl.4291.bc.norm.rv.RData", sep="")
+
+load(path.4291)
+
+gpl.4291.rv.M <- remove_bad_spots(gpl.4291.bc.norm.rv)
+gpl.4291.rv.A <- remove_bad_spots(gpl.4291.bc.norm.rv, type="A")
+
 
 #Get coefficient of variation of ratios of the the replicated probes
 #Remove bad arrays first
@@ -19,11 +31,54 @@ bad.4291 <- c("GSM237637.gpr", "GSM237644.gpr", "GSM237645.gpr", "GSM237646.gpr"
               "GSM237651.gpr", "GSM237655.gpr", "GSM237666.gpr", "GSM237667.gpr")
 
 good.4291.col.idx <- which(!(colnames(gpl.4291.rv.M) %in% bad.4291))
-gpl.4291.clean <- gpl.4291.rv.M[,good.4291.col.idx]
+gpl.4291.M <- gpl.4291.rv.M[,good.4291.col.idx]
+gpl.4291.A <- gpl.4291.rv.A[,good.4291.col.idx]
+
+gpl.4291.M$gene <- rownames(gpl.4291.M)
+gpl.4291.A$gene <- rownames(gpl.4291.A)
+
+replicated.4291 <- unique(gpl.4291.M[duplicated(gpl.4291.M$gene),]$gene)
+coefs.4291 <- sapply(replicated.4291, probe_CV, df=gpl.4291.M)
+mean.A.4291 <- sapply(replicated.4291, probe_mean_A, df=gpl.4291.A)
+mean.M.4291 <- sapply(replicated.4291, probe_mean_A, df=gpl.4291.M)
 
 
-replicated.4291 <- unique(gpl.4291.clean[duplicated(gpl.4291.clean$gene),]$gene)
-coefs.4291 <- sapply(replicated.4291, probe_CV, df=gpl.4291.rv.M)
+#########
+#
+#     Plots of CV vs M and A
+#
+#########
+
+#CV probably isn't a good measure to filter genes with technical replicate probes,
+#these plots will show that
+par(mfrow=c(2,2))
+
+#Plots of all the CVs vs M and A
+plot(unlist(mean.A.4291), unlist(coefs.4291),
+     main='CV as a function of A\nGPL4291',
+     xlab='1/2 * log2(R*G)',
+     ylab='CV')
+
+#This plot shows that the extreme values are all near 0
+#which makes sense given CV = SD/mean
+plot(unlist(mean.M.4291), unlist(coefs.4291),
+     main = 'CV as a function of log2 (R/G)\nGPL4291',
+     xlab='log2(R/G)',
+     ylab = 'CV')
+
+#Filter out extreme values to see if this trend is true across the data
+no_outliers.4291 <- which(abs(unlist(coefs.4291))<10)
+
+plot(unlist(mean.A.4291)[no_outliers.4291], unlist(coefs.4291)[no_outliers.4291],
+     main = 'CV as a function of A\nFiltered CV < 10\nGPL4291',
+     xlab='1/2 * log2(R*G)',
+     ylab = 'CV')
+
+plot(unlist(mean.M.4291)[no_outliers.4291], unlist(coefs.4291)[no_outliers.4291],
+     main = 'CV as a function of log2 (R/G)\nFiltered CV < 10\nGPL4291',
+     xlab='log2(R/G)',
+     ylab = 'CV')
+
 coefs.mean.4291 <- apply(coefs.4291, 2, mean)
 coefs.median.4291 <- apply(coefs.4291, 2, median)
 
