154e0074f59296f8338664b512b3419ec90e2322
Joe Reistetter <jreistetter@gmail.com>
2012-04-07 12:13:03 -0700

    GPL8561 arrays read in as MA objects

--- exprs/GSE8839_discretize.R
+++ exprs/GSE8839_discretize.R
@@ -6,3 +6,87 @@
 # 5 - Discretize ratios to {-1, 0 1} for the treatment arrays
 
 # Written by Joe Reistetter
+
+library(GEOquery)
+library(limma)
+
+#At OHSU Dropbox
+#setwd("/Domain/ohsum01.ohsu.edu/Users/reistett/Dropbox/thesis_work/")
+#My laptop Dropbox
+setwd("~/schoolDB/Dropbox/thesis_work")
+source("./code/exprs/exprs_funcs.R")
+
+#setwd("./data/exprs/GSE8839")
+
+#Pull info from the GEO repository so we can get annotations for each platform
+
+#Downloads the soft files
+g.8839.geo <- getGEO("GSE8839", destdir="./data/exprs/GSE8839", GSEMatrix=F) 
+g.8839.gsm <- GSMList(g.8839.geo)
+
+g.8839.geo.matrix <- getGEO("GSE8839", destdir="./data/exprs/GSE8839")
+
+#Function to pull out the data table and save as text
+save_table <- function(gsm, out_dir){
+  dat <- Table(gsm@dataTable)
+  accession <- gsm@header$geo_accession
+  fname <- paste(accession, ".txt", sep="")
+  write.table(dat, file=paste(out_dir,fname,sep=""), 
+              sep="\t", quote=F, col.names=T, row.names=F)
+  
+}
+
+dir.create("./data/exprs/GSE8839/GSE8839_RAW")
+#setwd("./GSE16146_RAW")
+
+#Save out to text
+lapply(g.8839.gsm, save_table, out_dir = "./data/exprs/GSE8839/GSE8839_RAW/")
+
+##########################
+
+#  GSE8839 - GPL8561
+
+##########################
+#First, need to create Targets frame for use in the read.maimages func
+
+#Gets phenotype data
+g.8561 <- g.8839.geo.matrix[['GSE8839-GPL8561_series_matrix.txt.gz']]
+g.8561.pheno <- phenoData(g.8561)
+g.8561.pdata <- pData(g.8561.pheno) #Dataframe of all data associated with arrays
+g.8561.annot <- featureData(g.8561)@data
+
+g.8561.fnames <- rownames(g.8561.pdata)
+g.8561.fnames <- sapply(g.8561.fnames, 
+                          function(x) {paste(x, ".txt", sep="")},
+                          USE.NAMES=F
+                          )
+
+#Get the GEO accessions for each array to use as an ID
+g.8561.arraynames <- rownames(g.8561.pdata)
+
+#Get the treatments for each array
+g.8561.treatment <- as.character(g.8561.pdata$source_name_ch2)
+
+#All the data is in the correct format, build it into a dataframe
+#that fits the specs for input into the read.maimages function.
+g.8561.targets <- data.frame(FileName=g.8561.fnames,
+                               Cy3 = rep("control", length(g.8561.fnames)),
+                               Cy5 = g.8561.treatment, stringsAsFactors=F)
+
+rownames(g.8561.targets) <- g.8561.arraynames
+
+##Set the column IDs for reading each dataset into the limma object
+#column names: Ch1 Intensity (Mean), Ch1 Background (Median), Ch2 Background (Median), Ch2 Intensity (Mean) -- ch1 = Cy3 = green, ch2 = Cy5 = red
+g.8561.cols <- list(R="CH2I_MEAN", G="CH1I_MEAN",
+                      Rb="CH2B_MEDIAN", Gb="CH1B_MEDIAN")
+
+#Define weight function to exclude spots that have a "FLAG" column value < 0 in the
+#raw data
+flagged <- function(x) as.numeric(x[,"FLAG"] > -1)
+
+g.8561.rg <- read.maimages(g.8561.targets,
+                             annotation=c("ID_REF"),
+                             columns=g.8561.cols,
+                             wt.fun=flagged,
+                             path="./data/exprs/GSE8839/GSE8839_RAW")
+
