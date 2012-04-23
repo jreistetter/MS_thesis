b4ae7d83911ac93dfda89ff9f55e80c9b14b98ee
Joe Reistetter <jreistetter@gmail.com>
2012-04-22 19:35:55 -0700

    First network made

--- expr_wgcna.R
+++ expr_wgcna.R
@@ -3,3 +3,28 @@
 # 
 # Written by Joe Reistetter
 
+#Load data
+#Laptop
+setwd("~/schoolDB/Dropbox/thesis_work/data/exprs/")
+
+load("expr.mean.RData")
+
+#Functions want samples as rows, genes as columns so need to transpose
+expr.t <- t(expr.mean)
+
+library(WGCNA)
+pickSoftThreshold(data=expr.mean, networkType="signed")
+
+library(procona)
+net <- newProconaObj(networkName="Mtb", 
+                     pepdat=expr.t, scaleFreeThreshold=0.7, signed="signed")
+
+save(net, file="WGCNA_net1.RData")
+
+That produces a network object with a bunch of different bits…
+in the terminal type "net@"  then hit the tab key a couple of times to 
+see them all.
+
+To get the number of modules formed:
+  
+  table(net@mergedColors)
