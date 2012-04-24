4d32e700086b8e169022f0c0e02b2a98d8748a66
Joe Reistetter <jreistetter@gmail.com>
2012-04-24 16:11:00 -0700

    parse_pathways parses in the pathways from PMN

--- PMN_output/4_17_30_mods_parse.py
+++ PMN_output/4_17_30_mods_parse.py
@@ -99,19 +99,44 @@ def parse_members(path, out):
                 mod_members.append(val)
 
         mod = GeneList(tuple([mod_id, mod_members]))
-        mod.print_mod(out_f)    
+        mod.print_mod(out_f)
+
+def parse_pathways(path, out):
+    f = open(path, 'r')
+    txt = f.read()
+    path_chunks = txt.strip().split('\n')
+    output = []
+
+    for chunk in path_chunks:
+        vals = chunk.split(':')
+        mod_id = vals[1].strip().split(' ')[1]
+
+        proteins = []
+        
+        for val in vals[0].split(' '):
+            if 'RV' in val:
+                #List has pRV0667, remove p and append
+                proteins.append(val[1:])
+
+        output.append(tuple([mod_id, proteins]))
+
+    return output
+            
+            
    
     
 def main():
     import os
     root_path = os.path.expanduser('~/Dropbox/thesis_work/PMN_output/')
     mods_path = root_path + '4.17.12.30_mods_output.txt'
+    protein_path = root_path + '4.17.12.30_mods_output_pathways.txt'
+    
     out_path = root_path + '4.17.30_mods_parsed.txt'
     members_out = root_path + '4.17.30_mods_members.txt'
     
+    
     parse_modules(mods_path, out_path)
     parse_members(mods_path, members_out)
-    
 
 
 if __name__ == '__main__':
