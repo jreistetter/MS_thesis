5646a9e3e5e2d278ebfa7597ba6eb98011127773
Joe Reistetter <jreistetter@gmail.com>
2012-04-29 12:31:14 -0700

    WGCNA functional enrichment finished and written to file

--- results/WGCNA_mod_func_enrich.R
+++ results/WGCNA_mod_func_enrich.R
@@ -73,10 +73,39 @@ load("GO/universe.RData")
 #Load the WGCNA modules
 load("exprs/filt_pt5.net.RData")
 
+mod_members.raw <- data.frame(moduleID=filt_pt5.net@mergedColors,
+                              gene=filt_pt5.net@peptides)
+dim(mod_members.raw)
+#[1] 2158    2
+
+#Filter out members not in the universe:
+mod_members <- mod_members.raw[mod_members.raw$gene %in% universe,]
+dim(mod_members)
+#[1] 1336    2, so ~800 have no associated GO term
+
+#Set the universe to be all genes present in the analysis that have GO terms
+my.universe <- mod_members$gene
+
+#Filter modules with the permutation test results
+perm.test <- filt_pt5.net@permtest
+good.mods <- perm.test[(perm.test[,5] < 0.005 & perm.test[,2] < 200),1]
+
+mod_members.good <- mod_members[mod_members$moduleID%in%good.mods,]
 
 
+all.mods.BP <- mod.hyper.batch(mod_members.good, my.universe, "BP")
+all.mods.CC <- mod.hyper.batch(mod_members.good, my.universe, "CC")
+all.mods.MF <- mod.hyper.batch(mod_members.good, my.universe, "MF")
 
+colnames(all.mods.BP)[1] <- "GO_ID"
+colnames(all.mods.CC)[1] <- "GO_ID"
+colnames(all.mods.MF)[1] <- "GO_ID"
 
+mods.GO.enrichment <- rbind(all.mods.BP, all.mods.CC)
+mods.GO.enrichment <- rbind(mods.GO.enrichment, all.mods.MF)
+
+write.table(mods.GO.enrichment, file="GO/WGCNA_func_enrichment.txt",
+            col.names=T, row.names=F, quote=F, sep='\t')
 
 
 
