a88b0cebe307f33f29a6b23f6bf749e101fd1938
Joe Reistetter <jreistetter@gmail.com>
2012-05-09 19:36:46 -0700

    Pathways annotated and saved

--- results/annotate_modules.R
+++ results/annotate_modules.R
@@ -70,7 +70,55 @@ colnames(modules.annotated) <- c("module", "parent", "gene")
 
 modules.annotated <- modules.annotated[with(modules.annotated, order(module, -parent)),]
 
-write.table(modules.annotated, "data/results/modules_annotated.txt",
+write.table(modules.annotated, "data/results/PMN_modules_annotated.txt",
             col.names=T, sep="\t", quote=F, row.names=F)
 
+##############################
+#
+#   Annotate the pathways
+#
+##############################
 
+path.raw <- read.table("PMN_output/4.17.30_mods_pathways_for_annotation.txt",
+                       head=F, sep=";")
+
+path.raw <- path.raw[path.raw[,1] %in% good.modules$moduleID,]
+
+path.allgenes <- unique(unlist(apply(path.raw, 1, function(x) strsplit(x[2], "\t", fixed=T))))
+
+genes.annot <- getBM(attributes=c("tuberculist", "external_gene_id"), 
+                     filters="tuberculist", 
+                     values=path.allgenes, mart=bac)
+
+genes.annot[,1] <- toupper(genes.annot[,1])
+
+paths.annot <- apply(path.raw, 1, function(row){
+  row.genes <- unlist(strsplit(row[2], "\t", fixed=T))
+  print(row.genes)
+  row.annotated <- vector(mode="character", length=length(row.genes))
+  for (i in c(1:length(row.genes))){
+    if(nrow(genes.annot[genes.annot[,1]==row.genes[i],])==0){
+      row.annotated[i] <- row.genes[i]
+    }
+        
+    else{
+    gene.name <- genes.annot[genes.annot[,1]==row.genes[i],2]
+    row.annotated[i] <- gene.name
+    }
+  }
+  return(row.annotated)
+}
+)
+
+
+pathways <- data.frame(moduleID=path.raw[,1])
+pathways$pathway <- ""
+
+for (i in c(1:nrow(pathways))){
+  pathways[i,2] <- paste(paths.annot[[i]], collapse=" -> ")
+}
+
+pathways <- pathways[with(pathways, order(moduleID)),]
+
+write.table(pathways, "data/results/PMN_module_pathways.txt",
+            row.names=F, col.names=T, sep="\t", quote=F)
